a11cfef2fe23c93889daca4b0f4eca151f3710a9
Nandini Agarwal <98901861+neophilic-coder@users.noreply.github.com>
2023-01-14 19:02:56 +0530

    Update Singly Linked List.c

--- Singly Linked List.c
+++ Singly Linked List.c
@@ -42,6 +42,98 @@ struct node* insertionatmid(struct node*f,int info,int index){
   return f;
 }
 
+//inserting an element at after a random node
+struct node* insertafternode(struct node* f, struct node* previousnode, int info){
+  struct node* ptr=(struct node*)malloc(sizeof(struct node));
+  ptr->info = info;
+  ptr->next = previousnode->next;
+  previousnode->next = ptr;
+  return f;
+}
+
+//searching an element in linked list
+struct node* searchnum(struct node* f,int num, int pos){
+  struct node* ptr=null;
+  ptr=f;
+  while(ptr!=null){
+    if(ptr->info==num){
+      printf("Search successful \nElement found at position: %d",pos);
+    }
+    pos=pos+1;
+    ptr=ptr->next;
+  }
+  return f;
+}
+
+//deleting the node at start of linked list
+struct node* deletionatfirst(struct node* f){
+  struct node* ptr=f;
+  f=f->next;
+  ptr->next=null;
+  free(ptr);
+  return f;
+}
+
+//deleting the node at the end of linked list
+struct node* deletionatend(struct node* f){
+  struct node* ptr=f;
+  struct node* temp = f->next;
+  while(ptr->next!=null){
+    ptr=ptr->next;
+    temp=temp->next;
+  }
+  ptr->next=null;
+  free(temp);
+  return f;
+}
+
+//deleting the node at an index of linked list
+struct node* deletionatindex(struct node* f,int index){
+   struct node *ptr=f;
+   struct node *q = f->next;
+   int i=0;
+   while(i!=(index-1))
+   {
+       ptr=ptr->next;
+       q=q->next;
+       i++;
+   }
+   ptr->next=q->next;
+   free(q);
+   return f;
+}
+
+// Deleting an element with the given value (which is also duplicate of its next one)
+struct node* deletionwithvalue(struct node* f,int value){
+    struct node *ptr=f;
+    struct node *q = f->next;
+    while(q->info!=value && q->next!=NULL)
+    {
+        ptr=ptr->next;
+        q=q->next;
+    }
+    if(q->info==value)
+    {
+        ptr->next=q->next;
+        free (q);
+    }
+    return f;
+}
+
+//reversing the singly linked list
+struct node* reverse(struct node *ptr){
+    struct node* pre=NULL;
+    struct node* after=NULL;
+    while(ptr!=NULL){
+        after=ptr->next;
+        ptr->next=pre;
+        pre=ptr;
+        ptr=after;
+    }
+    ptr=pre;
+    return;
+}
+
 //traversing the linked list and finding out its all elements
 void linkedlisttraversal(struct node* ptr){
   while(ptr!=null){
